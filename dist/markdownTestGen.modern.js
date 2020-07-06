b4a1280ba7e1b24e23a896d99b61bb0fdd20ae25
Ger Hobbelt <ger@hobbelt.com>
2020-07-06 02:29:47 +0200

    bumped build revision

--- dist/markdownTestGen.modern.js
+++ dist/markdownTestGen.modern.js
@@ -1,4 +1,4 @@
-/*! markdown-it-testgen 0.1.6-15 https://github.com//GerHobbelt/markdown-it-testgen @license MIT */
+/*! markdown-it-testgen 0.1.6-16 https://github.com//GerHobbelt/markdown-it-testgen @license MIT */
 
 let fs = require('fs');
 
